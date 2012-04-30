1df744e0e96f0092b8f009aba42e52edaf7178b2
SilverShadow <heilaterblanche@gmail.com>
2012-04-30 12:12:08 +0200

    source and receiver works, SenderReceiver tests them

---
+++ SenderReceiver.m
@@ -0,0 +1,22 @@
+%SenderReceiver test
+% setup workspace
+clc;
+clear;
+
+pktSize = 1500;
+genSize = 32;
+
+s1 = SourceNode_H('source1', genSize, pktSize);
+r1 = ReceiverNode_H('receiver1', genSize, pktSize);
+
+s1.sendFile('smiley.jpg');
+ 
+for i = 1:35
+    if (r1.ACK)
+        x = r1.sendPacket();
+        s1.receivePacket(x);
+    else
+        x = s1.sendPacket();
+        r1.receivePacket(x);    
+    end
+end
